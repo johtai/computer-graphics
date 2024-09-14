16b7894a638511b56eadaffd4a5724c562d13959
Tempire <danil.varlamov.03@mail.ru>
2024-09-14 13:45:27 +0300

    add task 1

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace ShadesOfGray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea11 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend11 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea12 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend12 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.openButton = new System.Windows.Forms.Button();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.openButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.5F);
+            this.openButton.Location = new System.Drawing.Point(431, 26);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(128, 57);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Открыть";
+            this.openButton.UseVisualStyleBackColor = false;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // convertButton
+            // 
+            this.convertButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.convertButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.convertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.convertButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.convertButton.Location = new System.Drawing.Point(812, 26);
+            this.convertButton.Margin = new System.Windows.Forms.Padding(0, 0, 0, 3);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(128, 57);
+            this.convertButton.TabIndex = 1;
+            this.convertButton.Text = "Преобразовать";
+            this.convertButton.UseVisualStyleBackColor = false;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(17, 103);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(328, 415);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(351, 103);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(337, 415);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(694, 103);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(350, 415);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 5;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Location = new System.Drawing.Point(1050, 103);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(350, 415);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // chart1
+            // 
+            chartArea11.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea11);
+            legend11.Name = "Legend1";
+            this.chart1.Legends.Add(legend11);
+            this.chart1.Location = new System.Drawing.Point(130, 546);
+            this.chart1.Name = "chart1";
+            series11.ChartArea = "ChartArea1";
+            series11.Legend = "Legend1";
+            series11.Name = "Оттенки серого 1";
+            this.chart1.Series.Add(series11);
+            this.chart1.Size = new System.Drawing.Size(480, 303);
+            this.chart1.TabIndex = 7;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea12.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea12);
+            legend12.Name = "Legend1";
+            this.chart2.Legends.Add(legend12);
+            this.chart2.Location = new System.Drawing.Point(812, 546);
+            this.chart2.Name = "chart2";
+            series12.ChartArea = "ChartArea1";
+            series12.Legend = "Legend1";
+            series12.Name = "Оттенки серого 2";
+            this.chart2.Series.Add(series12);
+            this.chart2.Size = new System.Drawing.Size(480, 303);
+            this.chart2.TabIndex = 8;
+            this.chart2.Text = "chart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1412, 895);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.openButton);
+            this.Name = "Form1";
+            this.Text = "Чёрно-белое изображение";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ShadesOfGray
+{
+    public partial class Form1 : Form
+    {
+        int[] hist1 = new int[256];
+        int[] hist2 = new int[256];
+
+        bool isOppened = false;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+
+
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            if (ofd.ShowDialog() == DialogResult.OK) 
+            {
+
+                try
+                {
+                    pictureBox1.Image = new Bitmap(ofd.FileName);
+                }
+                catch 
+                {
+                    MessageBox.Show("Невозможно открыть выбранный файл");
+                }
+                
+                isOppened = true;
+            }
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+
+            if(isOppened == false) 
+            {
+                MessageBox.Show("Нечего преобразовывать!");
+                return;
+            }
+
+
+            //NTSC RGB
+            Bitmap input_b = new Bitmap(pictureBox1.Image);
+            int Y;
+            for (int x = 0; x < input_b.Width; ++x) 
+            {
+
+                for (int y = 0; y < input_b.Height; ++y) 
+                {   
+                    
+                    Color pixelColor = input_b.GetPixel(x, y);
+                    Y = (int)(0.299 * pixelColor.R + 0.587 * pixelColor.G + 0.114 * pixelColor.B);
+                    Color newColor = Color.FromArgb(Y, Y, Y);
+                    input_b.SetPixel(x, y, newColor);
+
+                    hist1[Y] += 1;
+                }
+
+            }
+            pictureBox2.Image = input_b;
+
+            //sRGB.
+            Bitmap inputSRGB = new Bitmap(pictureBox1.Image);
+            for (int x = 0; x < inputSRGB.Width; x++)
+            {
+
+                for(int y = 0; y < inputSRGB.Height; y++) 
+                {
+
+                    Color pixelColor2 = inputSRGB.GetPixel(x, y);
+                    Y = (int)(0.2126 * pixelColor2.R + 0.7152 * pixelColor2.G + 0.0722 * pixelColor2.B);
+                    Color newColor2 = Color.FromArgb(Y, Y, Y);
+                    inputSRGB.SetPixel(x, y, newColor2);
+
+                    hist2[Y] += 1;
+                }
+
+            }
+            pictureBox3.Image = inputSRGB;
+
+            //Diffrend between NTSC RGB and sRGB.
+            Bitmap inputDiff = new Bitmap(pictureBox1.Image);
+            for (int x = 0; x < inputDiff.Width; x++) 
+            {
+
+                for (int y = 0; y < inputDiff.Height; y++) 
+                {
+
+                    Color pixelColorOne = input_b.GetPixel(x, y);
+                    Color pixelColorTwo = inputSRGB.GetPixel(x, y);
+                    int diff = Math.Abs(pixelColorOne.R - pixelColorTwo.R);
+                    Color newColor = Color.FromArgb(diff, diff, diff);
+                    inputDiff.SetPixel(x, y, newColor);
+                }
+
+            }
+
+            pictureBox4.Image = inputDiff;
+
+            //Рисуем Гистограмы для двух Цветовых схем 
+            DrawHystogram(hist1, chart1, "Оттенки серого 1");
+            DrawHystogram(hist2, chart2, "Оттенки серого 2");
+            
+        }
+
+
+
+        private void DrawHystogram(int[] histogram, Chart chart, string seriesName) 
+        {
+
+            chart.Series[seriesName].Points.Clear();
+            for (int i = 0; i < histogram.Length; i++) 
+            {
+
+                chart.Series[seriesName].Points.AddXY(i, histogram[i]);
+            }
+        }
+
+
+    }
+}
