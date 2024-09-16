4b43b7c58602e5e860389b230e825f96a61ec332
johtai <peterwaybright@gmail.com>
2024-09-17 00:07:15 +0300

    add task 2

---
+++ Lab2/Form2.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace ShadesOfGray
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button_LoadImage = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.button_red = new System.Windows.Forms.Button();
+            this.button_green = new System.Windows.Forms.Button();
+            this.button_blue = new System.Windows.Forms.Button();
+            this.chart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_LoadImage
+            // 
+            this.button_LoadImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_LoadImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button_LoadImage.Location = new System.Drawing.Point(938, 12);
+            this.button_LoadImage.Name = "button_LoadImage";
+            this.button_LoadImage.Size = new System.Drawing.Size(224, 87);
+            this.button_LoadImage.TabIndex = 0;
+            this.button_LoadImage.Text = "Загрузить изображение";
+            this.button_LoadImage.UseVisualStyleBackColor = true;
+            this.button_LoadImage.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(801, 581);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // button_red
+            // 
+            this.button_red.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_red.Enabled = false;
+            this.button_red.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button_red.Location = new System.Drawing.Point(938, 105);
+            this.button_red.Name = "button_red";
+            this.button_red.Size = new System.Drawing.Size(224, 62);
+            this.button_red.TabIndex = 2;
+            this.button_red.Text = "Выделить красный канал";
+            this.button_red.UseVisualStyleBackColor = true;
+            this.button_red.Click += new System.EventHandler(this.button_red_Click);
+            // 
+            // button_green
+            // 
+            this.button_green.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_green.Enabled = false;
+            this.button_green.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button_green.Location = new System.Drawing.Point(938, 173);
+            this.button_green.Name = "button_green";
+            this.button_green.Size = new System.Drawing.Size(224, 62);
+            this.button_green.TabIndex = 3;
+            this.button_green.Text = "Выделить зеленый канал";
+            this.button_green.UseVisualStyleBackColor = true;
+            this.button_green.Click += new System.EventHandler(this.button_green_Click);
+            // 
+            // button_blue
+            // 
+            this.button_blue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_blue.Enabled = false;
+            this.button_blue.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.button_blue.Location = new System.Drawing.Point(938, 241);
+            this.button_blue.Name = "button_blue";
+            this.button_blue.Size = new System.Drawing.Size(224, 62);
+            this.button_blue.TabIndex = 4;
+            this.button_blue.Text = "Выделить синий канал";
+            this.button_blue.UseVisualStyleBackColor = true;
+            this.button_blue.Click += new System.EventHandler(this.button_blue_Click);
+            // 
+            // chart
+            // 
+            this.chart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart.Legends.Add(legend1);
+            this.chart.Location = new System.Drawing.Point(834, 309);
+            this.chart.Name = "chart";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart.Series.Add(series1);
+            this.chart.Size = new System.Drawing.Size(328, 284);
+            this.chart.TabIndex = 5;
+            this.chart.Text = "chart";
+            // 
+            // Task2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1174, 605);
+            this.Controls.Add(this.chart);
+            this.Controls.Add(this.button_blue);
+            this.Controls.Add(this.button_green);
+            this.Controls.Add(this.button_red);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.button_LoadImage);
+            this.MinimumSize = new System.Drawing.Size(1076, 643);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Task2_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_LoadImage;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button button_red;
+        private System.Windows.Forms.Button button_green;
+        private System.Windows.Forms.Button button_blue;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart;
+    }
+}
---
+++ Lab2/Form2.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace ShadesOfGray
+{
+    public partial class Form2 : Form
+    {
+        private Bitmap image;
+        private Bitmap editedImage;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.jpg, *.png)|*.jpg;*.png";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Загрузка изображения
+                image = new Bitmap(openFileDialog.FileName);
+                pictureBox.Image = image;
+
+                // Создание Bitmap для редактирования и отображения
+                editedImage = new Bitmap(image.Width, image.Height);
+                button_red.Enabled = true;
+                button_green.Enabled = true;
+                button_blue.Enabled = true;
+            }
+        }
+
+        private void Task2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                Hide();
+            }
+        }
+
+        private void button_red_Click(object sender, EventArgs e)
+        {
+            ExtractChannel("red");
+        }
+
+        private void button_green_Click(object sender, EventArgs e)
+        {
+            ExtractChannel("green");
+        }
+
+        private void button_blue_Click(object sender, EventArgs e)
+        {
+            ExtractChannel("blue");
+        }
+
+        private void ExtractChannel(string color)
+        {
+            Console.WriteLine(color);
+            // Проход по каждому пикселю изображения
+            var fastBitmap = new Bitmap(image);
+            var fast_edited = new Bitmap(editedImage);
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    // Получение цвета пикселя
+                    Color pixelColor = fastBitmap.GetPixel(x, y);
+                    Color newColor = Color.White;
+                    switch (color)
+                    {
+                        case "red":
+                            newColor = Color.FromArgb(pixelColor.R, 0, 0);
+                            break;
+                        case "green":
+                            newColor = Color.FromArgb(0, pixelColor.G, 0);
+                            break;
+                        case "blue":
+                            newColor = Color.FromArgb(0, 0, pixelColor.B);
+                            break;
+                    }
+
+                    // Установка нового цвета для пикселя в отредактированном изображении
+                    fast_edited.SetPixel(x, y, newColor);
+                }
+            }
+                
+                //editedImage = e_image;
+            
+            GenerateColorHistogram(color, fast_edited);
+            // Отображение отредактированного изображения на PictureBox
+            pictureBox.Image = fast_edited;
+            pictureBox.Invalidate();
+        }
+
+        private void GenerateColorHistogram(string color, Bitmap fast_edited)
+        {
+            int[] histogram = new int[256]; // 256 possible color values
+            var fastBitmap = fast_edited;
+            
+            for (int x = 0; x < editedImage.Width; x++)
+            {
+                for (int y = 0; y < editedImage.Height; y++)
+                {
+                    Color pixel = fastBitmap.GetPixel(x, y);
+
+                    int value = 0;
+
+                    switch (color)
+                    {
+                        case "red":
+                            value = pixel.R;
+                            break;
+                        case "green":
+                            value = pixel.G;
+                            break;
+                        case "blue":
+                            value = pixel.B;
+                            break;
+                    }
+
+                    histogram[value]++;
+                }
+            }
+            
+            chart.Series[0].Points.Clear();
+            for (int i = 0; i < histogram.Length - 1; i++)
+            {
+                //Console.WriteLine($"{i} {histogram[i]}");
+                chart.Series[0].Points.Add(histogram[i]);
+            }
+            switch (color)
+            {
+                case "red":
+                    chart.Series[0].Color = Color.Red;
+                    break;
+                case "green":
+                    chart.Series[0].Color = Color.Green;
+                    break;
+                case "blue":
+                    chart.Series[0].Color = Color.Blue;
+                    break;
+            }
+            chart.Series[0].Name = color;
+        }
+
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
