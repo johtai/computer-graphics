6319efcbff40f9a338b9bb3c43455b5ac7d4f50e
johtai <peterwaybright@gmail.com>
2024-09-17 14:28:27 +0300

    add task 3

---
+++ Lab2/Form3.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace ShadesOfGray
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.button_LoadImage = new System.Windows.Forms.Button();
+            this.SaveImageButton = new System.Windows.Forms.Button();
+            this.HueLabel = new System.Windows.Forms.Label();
+            this.SaturationLabel = new System.Windows.Forms.Label();
+            this.ValueLabel = new System.Windows.Forms.Label();
+            this.hueTrackBar = new System.Windows.Forms.TrackBar();
+            this.saturationTrackBar = new System.Windows.Forms.TrackBar();
+            this.valueTrackBar = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hueTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.saturationTrackBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valueTrackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(612, 523);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // button_LoadImage
+            // 
+            this.button_LoadImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_LoadImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.button_LoadImage.Location = new System.Drawing.Point(676, 12);
+            this.button_LoadImage.Name = "button_LoadImage";
+            this.button_LoadImage.Size = new System.Drawing.Size(258, 87);
+            this.button_LoadImage.TabIndex = 3;
+            this.button_LoadImage.Text = "Загрузить изображение";
+            this.button_LoadImage.UseVisualStyleBackColor = true;
+            this.button_LoadImage.Click += new System.EventHandler(this.button_LoadImage_Click);
+            // 
+            // SaveImageButton
+            // 
+            this.SaveImageButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveImageButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SaveImageButton.Location = new System.Drawing.Point(676, 105);
+            this.SaveImageButton.Name = "SaveImageButton";
+            this.SaveImageButton.Size = new System.Drawing.Size(258, 87);
+            this.SaveImageButton.TabIndex = 4;
+            this.SaveImageButton.Text = "Сохранить ";
+            this.SaveImageButton.UseVisualStyleBackColor = true;
+            this.SaveImageButton.Click += new System.EventHandler(this.SaveImageButton_Click);
+            // 
+            // HueLabel
+            // 
+            this.HueLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.HueLabel.AutoSize = true;
+            this.HueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.HueLabel.Location = new System.Drawing.Point(671, 308);
+            this.HueLabel.Name = "HueLabel";
+            this.HueLabel.Size = new System.Drawing.Size(136, 29);
+            this.HueLabel.TabIndex = 6;
+            this.HueLabel.Text = "Оттенок: 0";
+            // 
+            // SaturationLabel
+            // 
+            this.SaturationLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaturationLabel.AutoSize = true;
+            this.SaturationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SaturationLabel.Location = new System.Drawing.Point(671, 388);
+            this.SaturationLabel.Name = "SaturationLabel";
+            this.SaturationLabel.Size = new System.Drawing.Size(212, 29);
+            this.SaturationLabel.TabIndex = 6;
+            this.SaturationLabel.Text = "Насыщенность: 0";
+            // 
+            // ValueLabel
+            // 
+            this.ValueLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ValueLabel.AutoSize = true;
+            this.ValueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ValueLabel.Location = new System.Drawing.Point(671, 459);
+            this.ValueLabel.Name = "ValueLabel";
+            this.ValueLabel.Size = new System.Drawing.Size(218, 29);
+            this.ValueLabel.TabIndex = 6;
+            this.ValueLabel.Text = "Интенсивность: 0";
+            // 
+            // hueTrackBar
+            // 
+            this.hueTrackBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.hueTrackBar.Location = new System.Drawing.Point(676, 340);
+            this.hueTrackBar.Maximum = 360;
+            this.hueTrackBar.Name = "hueTrackBar";
+            this.hueTrackBar.Size = new System.Drawing.Size(261, 45);
+            this.hueTrackBar.TabIndex = 7;
+            this.hueTrackBar.TickFrequency = 20;
+            this.hueTrackBar.Scroll += new System.EventHandler(this.hueTrackBar_Scroll);
+            this.hueTrackBar.MouseCaptureChanged += new System.EventHandler(this.hueTrackBar_MouseCaptureChanged);
+            // 
+            // saturationTrackBar
+            // 
+            this.saturationTrackBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.saturationTrackBar.Location = new System.Drawing.Point(676, 420);
+            this.saturationTrackBar.Maximum = 100;
+            this.saturationTrackBar.Name = "saturationTrackBar";
+            this.saturationTrackBar.Size = new System.Drawing.Size(261, 45);
+            this.saturationTrackBar.TabIndex = 7;
+            this.saturationTrackBar.TickFrequency = 5;
+            this.saturationTrackBar.Value = 50;
+            this.saturationTrackBar.Scroll += new System.EventHandler(this.saturationTrackBar_Scroll);
+            this.saturationTrackBar.MouseCaptureChanged += new System.EventHandler(this.saturationTrackBar_MouseCaptureChanged);
+            // 
+            // valueTrackBar
+            // 
+            this.valueTrackBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.valueTrackBar.Location = new System.Drawing.Point(676, 491);
+            this.valueTrackBar.Maximum = 100;
+            this.valueTrackBar.Name = "valueTrackBar";
+            this.valueTrackBar.Size = new System.Drawing.Size(261, 45);
+            this.valueTrackBar.TabIndex = 7;
+            this.valueTrackBar.TickFrequency = 5;
+            this.valueTrackBar.Value = 50;
+            this.valueTrackBar.Scroll += new System.EventHandler(this.valueTrackBar_Scroll);
+            this.valueTrackBar.MouseCaptureChanged += new System.EventHandler(this.valueTrackBar_MouseCaptureChanged);
+            // 
+            // Task3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(946, 547);
+            this.Controls.Add(this.valueTrackBar);
+            this.Controls.Add(this.saturationTrackBar);
+            this.Controls.Add(this.hueTrackBar);
+            this.Controls.Add(this.ValueLabel);
+            this.Controls.Add(this.SaturationLabel);
+            this.Controls.Add(this.HueLabel);
+            this.Controls.Add(this.SaveImageButton);
+            this.Controls.Add(this.button_LoadImage);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Task3";
+            this.Text = "Task3";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Task3_FormClosing);
+            this.Load += new System.EventHandler(this.Task3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hueTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.saturationTrackBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.valueTrackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button button_LoadImage;
+        private System.Windows.Forms.Button SaveImageButton;
+        private System.Windows.Forms.Label HueLabel;
+        private System.Windows.Forms.Label SaturationLabel;
+        private System.Windows.Forms.Label ValueLabel;
+        private System.Windows.Forms.TrackBar hueTrackBar;
+        private System.Windows.Forms.TrackBar saturationTrackBar;
+        private System.Windows.Forms.TrackBar valueTrackBar;
+    }
+}
---
+++ Lab2/Form3.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShadesOfGray
+{
+    public partial class Form3 : Form
+    {
+        private Bitmap source_image; //Bitmap для открываемого изображения
+        private Bitmap edited_image; //Bitmap для открываемого изображения
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Task3_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                Hide();
+            }
+        }
+
+        struct ColorHSV
+        {
+            public float hue;
+            public float saturation;
+            public float value;
+        }
+        ColorHSV[,] mapHSV;
+
+        private void button_LoadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open_dialog = new OpenFileDialog();
+            open_dialog.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG|All files (*.*)|*.*";
+
+            if (open_dialog.ShowDialog() == DialogResult.OK)
+            {
+                source_image = new Bitmap(open_dialog.FileName);
+                edited_image = new Bitmap(source_image.Width, source_image.Height);
+
+                pictureBox.Image = source_image;
+                pictureBox.Invalidate();
+
+                mapHSV = new ColorHSV[source_image.Width, source_image.Height];
+
+                var fastBitmap = new Bitmap(source_image);
+                    
+                for (var x = 0; x < fastBitmap.Width; ++x)
+                    for (var y = 0; y < fastBitmap.Height; ++y)
+                    {
+                        RGBToHSV(fastBitmap.GetPixel(x, y), out mapHSV[x, y].hue, out mapHSV[x, y].saturation, out mapHSV[x, y].value);
+                    }
+                    
+                ProcessImage();
+            }
+        }
+
+        float hue_change = 0;
+        float saturation_change = 0;
+        float value_change = 0;
+        private void ProcessImage()
+        {
+            var fastBitmap = new Bitmap(edited_image);
+            
+            for (var x = 0; x < fastBitmap.Width; ++x)
+                for (var y = 0; y < fastBitmap.Height; ++y)
+                {
+
+                    fastBitmap.SetPixel(x, y, HSVToRGB(mapHSV[x, y].hue + hue_change,
+                        Math.Min(1, Math.Max(0, mapHSV[x, y].saturation + saturation_change)),
+                        Math.Min(1, Math.Max(0, mapHSV[x, y].value + value_change))));
+                }
+            
+            pictureBox.Image = fastBitmap;
+            pictureBox.Invalidate();
+        }
+
+        private void RGBToHSV(Color color, out float hue, out float saturation, out float value)
+        {
+            float r = color.R / 255.0f;
+            float g = color.G / 255.0f;
+            float b = color.B / 255.0f;
+
+            float max = Math.Max(Math.Max(r, g), b);
+            float min = Math.Min(Math.Min(r, g), b);
+            hue = 0;
+
+            if (min == max)
+                hue = 0;
+            else if (max == r && g >= b)
+                hue = 60 * ((g - b) / (max - min)) + 0;
+            else if (max == r && g < b)
+                hue = 60 * ((g - b) / (max - min)) + 360;
+            else if (max == g)
+                hue = 60 * ((b - r) / (max - min)) + 120;
+            else if (max == b)
+                hue = 60 * ((r - g) / (max - min)) + 240;
+            hue %= 360;
+
+            value = max;
+
+            if (max == 0)
+                saturation = 0;
+            else
+                saturation = 1 - min / max;
+        }
+        private Color HSVToRGB(float hue, float saturation, float value)
+        {
+            int Hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            double f = hue / 60 - Math.Floor(hue / 60);
+            double v = value * 255;
+            int vInt = Convert.ToInt32(v);
+            int p = Convert.ToInt32(v * (1 - saturation));
+            int q = Convert.ToInt32(v * (1 - f * saturation));
+            int t = Convert.ToInt32(v * (1 - (1 - f) * saturation));
+
+            switch (Hi)
+            {
+                case 0:
+                    return Color.FromArgb(vInt, t, p);
+                case 1:
+                    return Color.FromArgb(q, vInt, p);
+                case 2:
+                    return Color.FromArgb(p, vInt, t);
+                case 3:
+                    return Color.FromArgb(p, q, vInt);
+                case 4:
+                    return Color.FromArgb(t, p, vInt);
+                default:
+                    return Color.FromArgb(vInt, p, q);
+            }
+        }
+        private void SaveImageButton_Click(object sender, EventArgs e)
+        {
+            if (edited_image == null)
+            {
+                MessageBox.Show("Сначала загрузите изображение");
+                return;
+            }
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "JPEG Image|*.jpg";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                edited_image.Save(saveFileDialog.FileName, ImageFormat.Jpeg);
+                MessageBox.Show("Изображение успешно сохранено");
+            }
+        }
+
+        private void hueTrackBar_Scroll(object sender, EventArgs e)
+        {
+            HueLabel.Text = "Оттенок: " + hueTrackBar.Value;
+            hue_change = hueTrackBar.Value;
+        }
+
+        private void saturationTrackBar_Scroll(object sender, EventArgs e)
+        {
+            SaturationLabel.Text = "Насыщенность: " + ((saturationTrackBar.Value - 50) / 50f).ToString();
+            saturation_change = (saturationTrackBar.Value - 50) / 50f;
+        }
+
+        private void valueTrackBar_Scroll(object sender, EventArgs e)
+        {
+            ValueLabel.Text = "Интенсивность: " + ((valueTrackBar.Value - 50) / 50f).ToString();
+            value_change = (valueTrackBar.Value - 50) / 50f;
+        }
+
+        private void hueTrackBar_MouseCaptureChanged(object sender, EventArgs e)
+        {
+            if (edited_image != null)
+                ProcessImage();
+        }
+
+        private void saturationTrackBar_MouseCaptureChanged(object sender, EventArgs e)
+        {
+            if (edited_image != null)
+                ProcessImage();
+        }
+
+        private void valueTrackBar_MouseCaptureChanged(object sender, EventArgs e)
+        {
+            if (edited_image != null)
+                ProcessImage();
+        }
+
+        private void Task3_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
